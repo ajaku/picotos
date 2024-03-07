08efc0917a2c9cdece17e3c67c7a12b0e8f7f388
ajaku <andyjaku13@gmail.com>
2024-03-07 00:08:55 -0500

    add basic rtos function

--- project-files/assembly.s
+++
@@ -1,9 +0,0 @@
-.global loop
-
-loop:
-    cpsid if
-
-    ldr r0, =#25
-    ldr r1, =#0
-    b my_gpio_put
-    cpsie if
--- project-files/main.c
+++ project-files/main.c
@@ -3,23 +3,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define GPIO_ON  1
-#define GPIO_OFF 0
-#define LED_PIN  25
 
-void loop();
-
-void my_gpio_put(int pin, int val) {
-    gpio_put(pin, val);
-    return;
-}
-
-int main(int argc, char **argv) {
-    stdio_init_all();
-
+void led_task()
+{   
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(100);
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(100);
+    }
+}
 
-    gpio_put(LED_PIN, GPIO_ON);
-    loop();
+int main()
+{
+    stdio_init_all();
+
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
+
+    while(1){};
 }
