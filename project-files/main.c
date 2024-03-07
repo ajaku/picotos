228ab9c88baa7179c5107d7f473c1fbc787ea0f2
ajaku <andyjaku13@gmail.com>
2024-03-07 17:32:30 -0500

    first commit to new branch

--- project-files/main.c
+++ project-files/main.c
@@ -1,25 +1,10 @@
-#include <FreeRTOS.h>
-#include <task.h>
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <FreeRTOS.h>
+#include <stdio.h>
+#include <task.h>
 
-#define GPIO_ON  1
+#define GPIO_ON 1
 #define GPIO_OFF 0
-#define LED_PIN  25
+#define LED_PIN 25
 
-void loop();
-
-void my_gpio_put(int pin, int val) {
-    gpio_put(pin, val);
-    return;
-}
-
-int main(int argc, char **argv) {
-    stdio_init_all();
-
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    gpio_put(LED_PIN, GPIO_ON);
-    loop();
-}
+int main(int argc, char **argv) { printf("Hello world\n"); }
