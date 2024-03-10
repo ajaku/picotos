43a19a6d148d87167c7ed4776c7eb1e7f68a7359
ajaku <andyjaku13@gmail.com>
2024-03-10 19:45:01 -0400

    update main

--- project-files/main.c
+++ project-files/main.c
@@ -1,24 +1,29 @@
-#include "pico/stdlib.h"
 #include <FreeRTOS.h>
-#include <stdio.h>
 #include <task.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
 
+void task_1(void *pvParameters) {
+    while (true) {
+        printf("Task 1 is running\n");
+        vTaskDelay(100);
+    }
+}
 
-#define LED_PIN 25
+void task_2(void *pvParameters) {
+    while (true) {
+        printf("Task 2 is running\n");
+        vTaskDelay(100);
+    }
+}
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(1000);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(1000);
-    }
+    xTaskCreate(task_1, "Task_1", 256, NULL, 1, NULL);
+    xTaskCreate(task_2, "Task_2", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
 
-    //xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    //vTaskStartScheduler();
-
-    //while(1){};
+    while(1){};
 }
