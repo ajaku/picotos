ad395470ddeedc8aa8c8c366b700826db6ef2f19
ajaku <andyjaku13@gmail.com>
2024-03-11 14:05:22 -0400

    merge changes

--- project-files/main.c
+++ project-files/main.c
@@ -1,11 +1,9 @@
 #include "pico/stdlib.h"
-#include <FreeRTOS.h>
-#include <stdio.h>
-#include <task.h>
 
 
 #define LED_PIN 25
 
+<<<<<<< Updated upstream
 int main()
 {
     stdio_init_all();
@@ -22,3 +20,17 @@ int main()
 
     //while(1){};
 }
+=======
+int main() { 
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (1) {
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(1000);
+    }
+}
+>>>>>>> Stashed changes
